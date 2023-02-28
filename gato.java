0542d287ddde260c0acf43abd4976f14dbc2d0b5
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-02-28 19:39:50 -0300

    Add files via upload

---
+++ gato.java
@@ -0,0 +1,61 @@
+package teste.intellij.primeiro.model;
+
+import java.util.Objects;
+
+public class gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public gato() {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        gato gato = (gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
---
+++ programa.java
@@ -0,0 +1,12 @@
+package teste.intellij.primeiro;
+
+import teste.intellij.primeiro.model.gato;
+
+public class programa {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+    }
+
+
+}
