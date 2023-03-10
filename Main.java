78f59d822e679944fe5f532f61594f14f5d44787
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-03-10 19:00:24 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,54 @@
-package set;
+package map;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+
     public static void main(String args[]){
-        Set<Carro> hashSetCarros = new HashSet<>();
 
-        //hash set
-        hashSetCarros.add(new Carro("Ford"));
-        hashSetCarros.add(new Carro("Chevrolet"));
-        hashSetCarros.add(new Carro("Fiat"));
-        hashSetCarros.add(new Carro("Peugeot"));
-        hashSetCarros.add(new Carro("Zip"));
-        hashSetCarros.add(new Carro("Alfa Romeo"));
 
-        System.out.println("Meu set: " + hashSetCarros);
+        //um map
+        Map<String, String> aluno = new HashMap<>();
+
+        //método put para adicionat itens
+        aluno.put("Nome", "João");
+        aluno.put("Idade", "17");
+        aluno.put("Media", "8.5");
+        aluno.put("Turma", "3a");
+
+        System.out.println("Dados do aluno: " + aluno);
         System.out.println("------");
 
-        //arvore set
-        Set<Carro> treeSetCarros = new TreeSet<>();
+        //key set: retorna todos os set que tenho para a classe aluno
+        System.out.println("Set's : " + aluno.keySet());
+        //values: retorna os valores de cada set
+        System.out.println("Valores: " + aluno.values());
+        System.out.println("------");
 
-        treeSetCarros.add(new Carro("Ford"));
-        treeSetCarros.add(new Carro("Chevrolet"));
-        treeSetCarros.add(new Carro("Fiat"));
-        treeSetCarros.add(new Carro("Peugeot"));
-        treeSetCarros.add(new Carro("Zip"));
-        treeSetCarros.add(new Carro("Alfa Romeo"));
+        //lista de map
+        List<Map<String, String>> listaAlunos = new ArrayList<>();
 
-        //mostra em ordem alfabetica (tamanho) devido ao método compareTo
-        System.out.println("Minha arvore set: " + treeSetCarros);
+        listaAlunos.add(aluno);
 
+
+        Map<String, String> aluno2 = new HashMap<>();
+
+        aluno2.put("Nome", "Maria");
+        aluno2.put("Idade", "18");
+        aluno2.put("Media", "8.9");
+        aluno2.put("Turma", "3b");
+
+        listaAlunos.add(aluno2);
+        System.out.println("Dados do aluno 2: " + aluno2);
+        System.out.println("------");
+
+        System.out.println("Lista de alunos: " + listaAlunos);
+        System.out.println("------");
+
+        //método containsKey: verifica se o map contem um set
+        System.out.println("Map contém set Nome? " + aluno.containsKey("Nome"));
     }
 }
