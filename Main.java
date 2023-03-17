c95d8fda930a95dcde3722b5e4da091a2153dc18
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-03-17 19:08:28 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,57 @@
-package Desafios.HcomNtermos;
+package Desafios.fibonacci;
 /*
-Neste desafio, faça um programa que calcule o valor de H com N termos.
-Sendo, H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N.
-A entrada consiste em um número inteiro positivo.
-Na saída será impresso o valor que representa a soma dos termos.
+A seguinte sequência de números 0 1 1 2 3 5 8 13 21... é conhecida como série de Fibonacci.
+Nessa sequência, cada número, depois dos 2 primeiros, é igual à soma dos 2 anteriores.
+Escreva um algoritmo que leia um inteiro N (N < 46) e mostre os N primeiros números dessa série.
+O arquivo de entrada contém um valor inteiro N (0 < N < 46).
+Os valores devem ser mostrados na mesma linha, separados por um espaço em branco.
+Não deve haver espaço após o último valor.
  */
 
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-
-    public static void main(String[] Args) {
-
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
         int numero = 0;
         int contador = 1;
-        double H = 0.0;
-        double divisao = 0;
+        int fibonacci = 0;
 
         System.out.println("Digite um número: ");
         numero = scan.nextInt();
 
-        do {
-            divisao = 1.0/contador;
-            H = H + divisao;
+        System.out.println("Sequencia Fibonacci = ");
 
-            contador++;
+        if(numero<=2) {
+            do {
+                System.out.print(" " + fibonacci);
+                fibonacci++;
+                contador++;
+            } while (contador <= numero);
+        }
 
-        }while(contador<=numero);
+            else{
+                System.out.print(0 + " " + 1);
 
-        System.out.println("Valor de H é: " + String.format("%.0f", H));
+                contador = 2;
+                int atual = 1;
+                int proximo = 0;
+                int anterior = 1;
+
+                do {
+                        System.out.print(" " + atual);
+
+                        proximo = atual + anterior;
+                        anterior = atual;
+                        atual = proximo;
+
+                        contador++;
+
+                    }while(contador<numero);
+            }
+
+
     }
 }
-
