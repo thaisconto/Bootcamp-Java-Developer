365c6d4caa2fb95258239bb4ffde40972519b6ba
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-05-05 10:12:04 -0300

    Create MainListaEncadeada.java

---
+++ MainListaEncadeada.java
@@ -0,0 +1,32 @@
+package listaEncadeada;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada();
+        minhaListaEncadeada.add("teste1");
+        minhaListaEncadeada.add("teste2");
+        minhaListaEncadeada.add("teste3");
+        minhaListaEncadeada.add("teste4");
+        System.out.println("Método get para cada posição:");
+        System.out.println("índice 0: " + (String)minhaListaEncadeada.get(0));
+        System.out.println("índice 1: " + (String)minhaListaEncadeada.get(1));
+        System.out.println("índice 2: " + (String)minhaListaEncadeada.get(2));
+        System.out.println("índice 3: " + (String)minhaListaEncadeada.get(3));
+        System.out.println("-------");
+        System.out.println("Minha lista: " + minhaListaEncadeada);
+        System.out.println("-------");
+        minhaListaEncadeada.remove(3);
+        System.out.println("Método remover índice 3: ");
+        System.out.println("Nova lista: " + minhaListaEncadeada);
+        System.out.println("-------");
+        System.out.println("Método size, tamanho da lista: " + minhaListaEncadeada.size());
+        System.out.println("-------");
+        System.out.println("Método get, adicionar teste 10: ");
+        minhaListaEncadeada.add("teste10");
+        System.out.println("Nova lista: " + minhaListaEncadeada);
+        System.out.println("-------");
+    }
+}
