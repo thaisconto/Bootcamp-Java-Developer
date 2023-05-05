8b8ac4401e694c912dca8eaec979f81fe188e9a2
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-05-05 10:17:36 -0300

    Create MainSet.java

---
+++ MainSet.java
@@ -0,0 +1,30 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Set<Carro> hashSetCarros = new HashSet();
+        hashSetCarros.add(new Carro("Ford"));
+        hashSetCarros.add(new Carro("Chevrolet"));
+        hashSetCarros.add(new Carro("Fiat"));
+        hashSetCarros.add(new Carro("Peugeot"));
+        hashSetCarros.add(new Carro("Zip"));
+        hashSetCarros.add(new Carro("Alfa Romeo"));
+        System.out.println("Meu set: " + hashSetCarros);
+        System.out.println("------");
+        Set<Carro> treeSetCarros = new TreeSet();
+        treeSetCarros.add(new Carro("Ford"));
+        treeSetCarros.add(new Carro("Chevrolet"));
+        treeSetCarros.add(new Carro("Fiat"));
+        treeSetCarros.add(new Carro("Peugeot"));
+        treeSetCarros.add(new Carro("Zip"));
+        treeSetCarros.add(new Carro("Alfa Romeo"));
+        System.out.println("Minha arvore set: " + treeSetCarros);
+    }
+}
