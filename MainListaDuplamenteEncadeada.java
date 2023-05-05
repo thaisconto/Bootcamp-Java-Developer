3005c2e120f760cdfe69f87b0a9a598247824715
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-05-05 10:12:40 -0300

    Create MainListaDuplamenteEncadeada.java

---
+++ MainListaDuplamenteEncadeada.java
@@ -0,0 +1,28 @@
+package listaDuplamenteEncadeada;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> minhaListaEncadeada = new ListaDuplamenteEncadeada();
+        minhaListaEncadeada.add("c1");
+        minhaListaEncadeada.add("c2");
+        minhaListaEncadeada.add("c3");
+        minhaListaEncadeada.add("c4");
+        minhaListaEncadeada.add("c5");
+        minhaListaEncadeada.add("c6");
+        minhaListaEncadeada.add("c7");
+        System.out.println("Minha lista: " + minhaListaEncadeada);
+        System.out.println("-----------");
+        System.out.println("Remover c4, índice 3");
+        minhaListaEncadeada.remove(3);
+        System.out.println(minhaListaEncadeada);
+        System.out.println("-----------");
+        System.out.println("Adicionar 99 a posição 4 (índice 3)");
+        minhaListaEncadeada.add(3, "99");
+        System.out.println(minhaListaEncadeada);
+        System.out.println("-----------");
+        System.out.println("Tamanho da lista: " + minhaListaEncadeada.size());
+    }
+}
