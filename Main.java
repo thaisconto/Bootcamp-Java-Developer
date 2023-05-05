20ce377bd4078151b0978a2ac22905cc439c3311
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-05-05 09:59:54 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,68 +1,58 @@
-package Desafios.EspacosEVogais;
+package Desafios.Triangulo;
 
+import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 /*
-Jorginho é professor do primário de uma determinada escola. Ele tem 100.000 alunos e precisa criar
-um programa que verifica quantos espaços em branco e quantas vogais existem em uma determinada string
-de entrada que os alunos entregaram na avaliação final. Ajude-o a realizar essa tarefa antes que o
-tempo para entrega-la no fim do semestre acabe!
-
- A entrada será uma frase no formato de string
- A saída deverá retornar quantos espaços em branco e quantas vogais existem na
- determinada string, conforme exemplo abaixo:
-"Esse desafio foi facil" - Espacos em branco: 3   Vogais: 10
- “Navegar nas aguas do teu mar” - Espacos em branco: 5 Vogais: 11
+Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo.
+Em caso positivo, calcule o perímetro do triângulo (soma de todos os lados) e apresente a mensagem: Perimetro = XX.X
+Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura,
+mostrando a mensagem: Area = XX.X
+Fórmula da área de um trapézio: AREA = ((A + B) x C) / 2
+O resultado deve ser apresentado com uma casa decimal.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite uma frase: ");
-        String frase = scan.nextLine();
+        System.out.println("Digite 3 valores reais:");
+        double A = scan.nextDouble();
+        double B = scan.nextDouble();
+        double C = scan.nextDouble();
 
-        //split separa a sting em palavras com regex
-        String[] fraseSplit = frase.split(" ");
-
-        //criando uma char com as vogais
-        char[] Vogais = {'a', 'e', 'i', 'o', 'u'};
-
-        //como a fraseSplit está separada em palavras, o espço em branco será palavras -1
-        int espacosEmBranco = fraseSplit.length - 1, quantVogais = 0;
-
-        //percorrendo a string em cada item -> palavra
-        for (String item : fraseSplit) {
-            //adicionando um contado i com length (número de cacateres dentro da string)
-            for (int i = 0; i < item.length(); i++){
-                //charAt retorna o caractere da posição i
-                char c = item.charAt(i);
-                //converter o item c em minusculo, para comprar com as vogais contidas na char
-                char cMinusculo = Character.toLowerCase(c);
-                //usando método contem (criado abaixo) para percorrer char vogal
-                if (contem(Vogais, cMinusculo))
-                    quantVogais++;
+        if (A >= B & A >=C){ //A é o maior
+            if(B + C > A){ //é um triangulo
+                double perimetro = A+B+C;
+                System.out.println("O perímetro do triângulo é " + String.format("%.1f",perimetro));
+            }else{
+                double areaTrapezio = ((A + B) * C) / 2;
+                System.out.println("Área do trapézio é " + String.format("%.1f",areaTrapezio));
             }
         }
 
-        System.out.println("Espacos em branco: " + espacosEmBranco + " Vogais: " + quantVogais);
-    }
-
-    //criando método contém: uma comparação dentro da char vogais
-
-    //vetor: é a minha char vogais, comparação é o meu item caractere c na posição i
-        private static boolean contem (char vetor[], char comparacao){
-
-        boolean flag = false;
-
-        //percorrer meu vetor vogais e retornar true se a comparação for positiva
-        for (int i = 0; i < vetor.length; i++){
-            if (vetor[i] == comparacao)
-                flag = true;
+        if (B >= C & B >=A){ //B é o maior
+            if(A + C > B){ //é um triangulo
+                double perimetro = A+B+C;
+                System.out.println("O perímetro do triângulo é " + String.format("%.1f",perimetro));
+            }else{
+                double areaTrapezio = ((A + B) * C) / 2;
+                System.out.println("Área do trapézio é " + String.format("%.1f",areaTrapezio));
+            }
         }
 
-        return flag;
+        if (C >= B & C >=A){ //C é o maior
+            if(B + C > C){ //é um triangulo
+                double perimetro = A+B+C;
+                System.out.println("O perímetro do triângulo é " + String.format("%.1f",perimetro));
+            }else{
+                double areaTrapezio = ((A + B) * C) / 2;
+                System.out.println("Área do trapézio é " + String.format("%.1f",areaTrapezio));
+            }
+        }
+/* como limitar casa decimal:
+// %.2f - pega o argumento e imprime com 2 casas depois da vírgula.
+   %n - passa para a próxima linha (equivalente ao println()).
+*/
 
-
-    }
-
-}
+}}
